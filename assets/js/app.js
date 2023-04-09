f4c97504039b5960d8fbe78c09db70e9ab24b815
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2023-04-09 23:32:17 +0900

    ローカルストレージの実装

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+// 初期表示
+window.onload = function(){
+
+    // ローカルストレージに保管されたデータの取得
+    const storageEmail = localStorage.getItem("userEmail");
+    const storagePassword = localStorage.getItem("userPassword");
+
+    // DOM
+    const userEmail = document.getElementById("input-email");
+    const userPassword = document.getElementById("input-password");
+
+    //ローカルストレージのemailとpasswordをセットする
+    if(storageEmail != null){
+        userEmail.value = storageEmail;
+        console.log(storageEmail);
+    }
+    if(storagePassword != null){
+        userPassword.value = storagePassword;
+        console.log(storagePassword);
+    }
+};
+
+// ボタンクリックアクション
+const btn =document.getElementById("sign-in-button");
+btn.addEventListener("click",function(){
+
+//　フォームに入力された値の取得
+const formEmail = document.getElementById("input-email").value;
+const formPassword = document.getElementById("input-password").value;
+
+// ブラウザ上にデータの保存
+localStorage.setItem("userEmail", formEmail);
+localStorage.setItem("userPassword", formPassword);
+},false);
